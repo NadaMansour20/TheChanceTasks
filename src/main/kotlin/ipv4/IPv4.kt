165c354b083b589480db9e4e80cbf7431cb5ae80
nada <nadamansoursamir@gmail.com>
2025-04-02 23:59:31 +0200

    Logic for IPv4 Problems

--- src/main/kotlin/ipv4/IPv4.kt
+++ src/main/kotlin/ipv4/IPv4.kt
@@ -4,7 +4,26 @@ package ipv4
 fun isValidIPv4(address:String):Boolean{
 
 
+    val segments=address.split('.')
 
-    return false
+    if(segments.size!=4)return false
+
+    for(segment in segments) {
+
+        val digit=segment.toIntOrNull()
+
+        if (digit != null) {
+            if(digit <0 || digit>255)return false
+        }
+
+        if(segment.isEmpty())return false
+
+        if(segment.length>1&&segment.startsWith('0'))return false
+
+        if(!segment.all { it.isDigit() })return false
+
+    }
+
+    return true
 
 }
